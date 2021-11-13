74809b5c11298a0ffbd6c55595c20dc54a45b53b
redbor <redbor24@gmail.com>
2021-11-13 11:48:07 +0300

    Мой вариант кода

--- guess.py
+++ guess.py
@@ -1,70 +1,80 @@
 import os
-
 from random import choice
 from string import ascii_lowercase
 
-FILENAME = 'sowpods.txt'
-os.chdir(os.path.dirname(__file__))
+DICT_SUBFOLDER = 'data'
+DICT_FILENAME = 'sowpods.txt'
 
 
-def get_rand_word(filename):
-    with open(filename, 'r') as f:
+def get_rand_word(dict_filename):
+    with open(dict_filename, 'r') as f:
         return choice([x.strip() for x in f]).lower()
 
 
-def print_game(wrong, word, letters):
-    print('\nWrong guess:', ', '.join(wrong))
-    for i in letters:
-        if i is None:
-            print(' ', end=' ')
-        else:
-            print(i, end=' ')
-    print()
-    for i in word:
-        if i != ' ':
-            print('-', end=' ')
-        else:
-            print(' ', end=' ')
-    print()
+def print_game(guessed_word, wrong_letters, guessed_word_letters):
+    print('-----------------------------------------------------------')
+    print('Wrong guess:', ', '.join(wrong_letters))
+
+    output_line = ''
+    for i in guessed_word_letters:
+        output_line += '  ' if i is None else i + ' '
+    print(output_line)
+
+    print('_ ' * len(guessed_word))
 
 
-def get_letter(attempt, guess):
-    user_input = input(f'> Guess a letter ({attempt} attempts left): ').lower()
+def get_letter(attempts, guess):
+    hint = 'english lowercase'
+    user_input = input(f'> Guess a {hint} letter ('
+                       f'{attempts} attempts left): ').lower()
+
     if len(user_input) != 1 or user_input not in ascii_lowercase:
-        print("> Please input a letter!")
+        print(f'> Please input a {hint} letter!')
+        return None
     elif user_input in guess:
         print(f"> You have guessed '{user_input}' before")
+        return None
     else:
         return user_input
 
 
-def play(word):
-    attempt = 6
-    guess, guess_wrong = [], []
-    letters = []
-    for i in word:
-        i = None if i != ' ' else i
-        letters.append(i)
-    while attempt > 0:
-        print_game(guess_wrong, word, letters)
-        if None not in letters:
+def play(hidden_word, attempts):
+    guess, wrong_guess = [], []
+    guessed_letters = [None] * len(hidden_word)
+
+    while attempts > 0:
+        print_game(hidden_word, wrong_guess, guessed_letters)
+
+        if None not in guessed_letters:
             print('> You win!')
-            break
-        user_letter = get_letter(attempt, guess)
-        if user_letter is not None:
-            for i in range(len(word)):
-                if user_letter == word[i]:
-                    letters[i] = user_letter
-            guess.append(user_letter)
-            if user_letter not in letters:
-                guess_wrong.append(user_letter)
-                attempt -= 1
-    else:
-        print_game(guess_wrong, word, letters)
-        print('> You lose...')
-        print(f'> Answer: {word.capitalize()}')
+            return
+
+        user_letter = get_letter(attempts, guess)
+
+        if user_letter is None:
+            continue
+        for letter_index, letter in enumerate(hidden_word):
+            if user_letter == letter:
+                guessed_letters[letter_index] = user_letter
+        guess.append(user_letter)
+
+        if user_letter in guessed_letters:
+            continue
+        wrong_guess.append(user_letter)
+        attempts -= 1
+
+    print_game(wrong_guess, hidden_word, guessed_letters)
+    print('> You lose...')
+    print(f'> Answer: {hidden_word.capitalize()}')
 
 
 if __name__ == "__main__":
-    word = get_rand_word(os.path.join('data', FILENAME))
-    play(word)
+    dict_filename = os.path.join(DICT_SUBFOLDER, DICT_FILENAME)
+    try:
+        play(get_rand_word(dict_filename), 6)
+    except FileNotFoundError:
+        print(f'Error! Dictionary {DICT_FILENAME} not found!'
+              f' Create {dict_filename} in working folder before playing')
+    except IndexError:
+        print(f'Error! Dictionary {DICT_FILENAME} is empty!'
+              'Fill it with words before playing')
